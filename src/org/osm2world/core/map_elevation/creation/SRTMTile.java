8ac40c8a178bbd71898af9c43442b2b3714a357c
Diego Carrión Robles <diego.carrion@navilo.es>
2018-04-30 17:32:24 +0200

    Restoring old tile SRTM to cope with crashes

--- src/org/osm2world/core/map_elevation/creation/SRTMTile.java
+++ src/org/osm2world/core/map_elevation/creation/SRTMTile.java
@@ -19,12 +19,8 @@ class SRTMTile {
 	/** value indicating a lack of data */
 	public static final short BLANK_VALUE = -32768;
 	
-	/** Possible values for the number of pixels of the SRTM tile */
-	static final int N_PIXELS_30M = 3601;
-	static final int N_PIXELS_90M = 1201;
-	
 	/** length of each dimension of an SRTM tile in pixels */
-	static int PIXELS = N_PIXELS_90M; //Safety net
+	static final int PIXELS = 1201;
 		
 	public final File file;
 	private final ShortBuffer data;
@@ -41,22 +37,6 @@ class SRTMTile {
 
 		FileInputStream fis = new FileInputStream(file);
 		FileChannel fc = fis.getChannel();
-		//Set dimension size depending on size of SRTM file:
-		switch ((int)fc.size()) {
-			case N_PIXELS_30M * N_PIXELS_30M * 2:
-				PIXELS = N_PIXELS_30M;
-				System.out.println("SRTM file contains 30m data");
-				break;
-			case N_PIXELS_90M * N_PIXELS_90M * 2:
-				System.out.println("SRTM file contains 90m data");
-				PIXELS = N_PIXELS_90M;
-				break;
-			default:
-				System.err.println("Warning: size of SRTM file: " + file.getName() + " is: " + fc.size() + " bytes, which doesn't correspond with 30m or 90m SRTM data");
-				fc.close();
-				fis.close();
-				throw new IOException();
-		}
 		ByteBuffer bb = ByteBuffer.allocateDirect((int) fc.size());
 		while (bb.remaining() > 0) fc.read(bb);
 		fc.close();
@@ -71,7 +51,7 @@ class SRTMTile {
 	
 	public final short getData(int x, int y) {
 		assert 0 <= x && x < PIXELS && 0 <= y && y < PIXELS;
-		return data.get((PIXELS - 1 - y) * PIXELS + x);
+		return data.get((1200 - y) * 1201 + x);
 	}
 	
 	@Override
